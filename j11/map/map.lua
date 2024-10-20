9937c90a4c27474269991e0b55f2c30ead65999c
DWTwilight <446416074@qq.com>
2024-10-20 22:17:07 +0800

    sperate map render

--- j11/map/map.lua
+++ j11/map/map.lua
@@ -4,10 +4,9 @@ MIN = M.min
 
 S = screen
 DM = S.drawMap
-DR = S.drawRect
 DRF = S.drawRectF
+DT = S.drawText
 DL = S.drawLine
-DC = S.drawCircle
 
 IN = input.getNumber
 IB = input.getBool
@@ -16,62 +15,6 @@ ON = output.setNumber
 P = property
 PN = P.getNumber
 PT = P.getText
-
-function dash(a, b)
-    DL(a, b + 2, a + 3, b + 2)
-end
-
-function plus(a, b)
-    DL(a, b + 2, a + 3, b + 2)
-    DL(a + 1, b + 1, a + 1, b + 2)
-    DL(a + 1, b + 3, a + 1, b + 4)
-end
-
-function RR(a, b)
-    DC(a + 1, b + 1, 1)
-    DL(a, b, a, b + 5)
-    DL(a + 2, b + 3, a + 2, b + 5)
-end
-
-function CST(a, b, s)
-    if s == "-" then
-        dash(a, b)
-    elseif s == "+" then
-        plus(a, b)
-    elseif s == "R" then
-        RR(a, b)
-    end
-end
-
-function STLen(s)
-    local len = 0
-    for d = 1, string.len(s) do
-        local sin = s:sub(d, d)
-        if len ~= 0 then
-            len = len + 1
-        end
-        if sin == "." or sin == " " then
-            len = len + 1
-        else
-            len = len + 3
-        end
-    end
-    return len
-end
-
-function DST(a, b, c)
-    a = math.floor(a)
-    b = math.floor(b)
-    for d = 1, string.len(c) do
-        local sin = c:sub(d, d)
-        CST(a, b, sin)
-        if sin == "." or sin == " " then
-            a = a + 2
-        else
-            a = a + 4
-        end
-    end
-end
 
 function H2RGB(e)
     e = e:gsub("#", "")
@@ -91,19 +34,21 @@ function PIR(x, y, rectX, rectY, rectW, rectH)
     return x >= rectX and y > rectY and x < rectX + rectW and y <= rectY + rectH
 end
 
-function PushButton(x, y, w, h, text, color, pressColor, visible)
+function PushButton(x, y, w, h, text, color, pressColor, visible, tox, toy, df)
     return {
         x = x,
         y = y,
         w = w,
         h = h,
         text = text,
-        tlen = STLen(text),
         c = color,
         pc = pressColor,
         pressed = false,
         onPress = false,
         v = visible,
+        tox = tox,
+        toy = toy,
+        df = df,
         press = function(btn, tx, ty)
             if btn.v then
                 local pressed = PIR(tx, ty, btn.x, btn.y, btn.w, btn.h)
@@ -129,7 +74,11 @@ function PushButton(x, y, w, h, text, color, pressColor, visible)
                 SC(c1)
                 DRF(btn.x, btn.y, btn.w, btn.h)
                 SC(c2)
-                DST(btn.x + btn.w // 2 - btn.tlen // 2, btn.y + (btn.h - 5) / 2, btn.text)
+                if df then
+                    df(btn.x + btn.tox, btn.y + btn.toy)
+                else
+                    DT(btn.x + btn.tox, btn.y + btn.toy, btn.text)
+                end
             end
         end
     }
@@ -143,13 +92,25 @@ TOUCH_FLAG = false
 UC = H2RGB(PT("UI Primary Color"))
 UC2 = H2RGB(PT("UI Secondary Color"))
 SCR_W, SCR_H = PN("Screen Width"), PN("Screen Height")
+DW, DH = PN("Display Width"), PN("Display Height")
 ZOOM_F = PN("Zoom Sensitivity")
 SMOOTH_F = PN("Smooth Factor")
 
+-- button custom draw
+function DZoomIn(x, y)
+    DL(x, y + 2, x + 5, y + 2)
+    DL(x + 2, y, x + 2, y + 2)
+    DL(x + 2, y + 3, x + 2, y + 5)
+end
+
+function DZoomOut(x, y)
+    DL(x, y + 2, x + 5, y + 2)
+end
+
 -- buttons
-ZOOM_IN_BTN = PushButton(SCR_W - 11, SCR_H - 5, 5, 5, "+", UC, UC2, true)
-ZOOM_OUT_BTN = PushButton(SCR_W - 5, SCR_H - 5, 5, 5, "-", UC, UC2, true)
-RESET_BTN = PushButton(SCR_W - 7, 0, 7, 7, "R", UC, UC2, false)
+ZOOM_IN_BTN = PushButton(SCR_W - 17, SCR_H - 9, 7, 7, "+", UC, UC2, true, 1, 1, DZoomIn)
+ZOOM_OUT_BTN = PushButton(SCR_W - 9, SCR_H - 9, 7, 7, "-", UC, UC2, true, 1, 1, DZoomOut)
+RESET_BTN = PushButton(SCR_W - 10, 2, 8, 9, "R", UC, UC2, false, 2, 2)
 BTNS = { ZOOM_IN_BTN, ZOOM_OUT_BTN, RESET_BTN }
 
 function clamp(value, min, max)
@@ -164,10 +125,14 @@ function calZoom(z)
     return 49.9 * (z - 1) ^ 2 + 0.1
 end
 
+function transformTouchInput(tx, ty)
+    return tx * SCR_W / DW + (SCR_W / DW) // 3, ty * SCR_H / DH + (SCR_H / DH) // 3
+end
+
 function onTick()
     -- handle touch input
     if IB(1) then
-        local tx, ty = IN(4), IN(5)
+        local tx, ty = transformTouchInput(IN(4), IN(5))
 
         for _, btn in ipairs(BTNS) do
             btn:press(tx, ty)
