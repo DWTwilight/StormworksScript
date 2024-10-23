5ff1ce997c5c749fcf72a6d436d4a265fbeef83c
DWTwilight <446416074@qq.com>
2024-10-24 00:44:25 +0800

    filter tags

--- addons/CustomRadar.lua
+++ addons/CustomRadar.lua
@@ -3,12 +3,15 @@ g_savedata = {}
 
 TARGET_DIAL_NAME = "[CR] targetId"
 
-function vehicle(groupId, hasRadar, targetId)
+EXCLUDE_TAGS = { "trade", "resource", "storage" }
+
+function vehicle(groupId, hasRadar, targetId, tag)
     return {
         groupId = groupId,
         matrix = nil,
         hasRadar = hasRadar,
-        targetId = targetId
+        targetId = targetId,
+        tag = tag
     }
 end
 
@@ -17,9 +20,17 @@ VEHICLES = {}
 
 function onVehicleSpawn(vehicle_id, peer_id, x, y, z, group_cost, group_id)
     VEHICLE_DATA, is_success = server.getVehicleData(vehicle_id)
-    if VEHICLE_DATA ~= nil then
-        if not VEHICLE_DATA["static"] then
-            VEHICLES[vehicle_id] = vehicle(group_id, false, nil)
+    if VEHICLE_DATA ~= nil and not VEHICLE_DATA["static"] then
+        local fullTag = VEHICLE_DATA["tags_full"]
+        local flag = true
+        for _, tag in ipairs(EXCLUDE_TAGS) do
+            if string.find(fullTag, tag) then
+                flag = false
+                break
+            end
+        end
+        if flag then
+            VEHICLES[vehicle_id] = vehicle(group_id, false, nil, fullTag)
         end
     end
 end
@@ -51,10 +62,11 @@ function onCustomCommand(full_message, user_peer_id, is_admin, is_auth, command,
             server.announce("[Custom Radar]", "current vehicles:")
             for vehicle_id, v in pairs(VEHICLES) do
                 server.announce("[Custom Radar]",
-                    string.format("id: %d, groupId: %d, hasRadar: %s, targetId: %d",
+                    string.format("id: %d, groupId: %d, hasRadar: %s, targetId: %d, tag: %s",
                         vehicle_id, v.groupId,
                         tostring(v.hasRadar),
-                        v.hasRadar and v.targetId or -999))
+                        v.hasRadar and v.targetId or -999,
+                        v.tag))
             end
         end
     end
---
+++ addons/CustomRadar.md
@@ -0,0 +1,4 @@
+# Custom Radar Addon
+
+## tag exclude
+
