8bf61654beae7492bb5174b55304e3cb6fabc275
DWTwilight <446416074@qq.com>
2024-11-14 00:28:10 +0800

    complete bc logic

--- j11/WeaponSystem/GSH_30_1/BallisticComputer.lua
+++ j11/WeaponSystem/GSH_30_1/BallisticComputer.lua
@@ -1,8 +1,13 @@
+-- for test
+-- require("new.j11.WeaponSystem.GSH_30_1.BallisticComputerTestData")
+
 M = math
 cos = M.cos
 sin = M.sin
 atan = M.atan
 abs = M.abs
+acos = M.acos
+exp = M.exp
 
 IN = input.getNumber
 IB = input.getBool
@@ -53,102 +58,134 @@ MUZZEL_VELOCITY = PN("[BC]Muzzel Velocity")
 GRAVITY = PN("[BC]Gravity")
 PRECISION = PN("[BC]Precision")
 MAX_ITER_COUNT = PN("[BC]Max Iterations")
-BARREL_HORIZONTAL_OFFSET = PN("[BC]Barrel Horizontal Offset")
-BARREL_VERTICAL_OFFSET = PN("[BC]Barrel Vertical Offset")
+MAX_TICK = PN("[BC]Max Tick")
+TARGET_DELAY_COMPENSATION = PN("[BC]Target Delay Compensation")
+SELF_DELAY_COMPENSATION = PN("[BC]Self Delay Compensation")
 
-function calculateVelocity(x, y, z, tick, windSpeedX, windSpeedZ, selfSpeedX, selfSpeedZ, selfSpeedY)
+function calculateVelocity(x, y, z, tick, windSpeedX, windSpeedZ)
     local kt = DRAG_COEFFICIENT * tick / TICK_PER_SEC
     local k = DRAG_COEFFICIENT
-    local velocityZ = (k * z - kt * windSpeedZ) / (1 - math.exp(-kt)) + windSpeedZ
-    local velocityY = (k * y + GRAVITY * tick / TICK_PER_SEC) / (1 - math.exp(-kt)) - GRAVITY / k
-    local velocityX = (k * x - kt * windSpeedX) / (1 - math.exp(-kt)) + windSpeedX
-    return velocityX - selfSpeedX, velocityY - selfSpeedY, velocityZ - selfSpeedZ
+    local velocityZ = (k * z - kt * windSpeedZ) / (1 - exp(-kt)) + windSpeedZ
+    local velocityY = (k * y + GRAVITY * tick / TICK_PER_SEC) / (1 - exp(-kt)) - GRAVITY / k
+    local velocityX = (k * x - kt * windSpeedX) / (1 - exp(-kt)) + windSpeedX
+    return velocityX, velocityY, velocityZ
 end
 
-function calculateGap(muzzelVelocity, x, y, z, tick, windSpeedX, windSpeedZ, selfSpeedX, selfSpeedZ, selfSpeedY)
-    local velocityX, velocityY, velocityZ = calculateVelocity(x, y, z, tick, windSpeedX, windSpeedZ, selfSpeedX,
-        selfSpeedZ, selfSpeedY)
+function calculateGap(muzzelVelocity, x, y, z, tick, windSpeedX, windSpeedZ)
+    local velocityX, velocityY, velocityZ = calculateVelocity(x, y, z, tick, windSpeedX, windSpeedZ)
     return muzzelVelocity - (velocityX ^ 2 + velocityY ^ 2 + velocityZ ^ 2) ^ 0.5
 end
 
-function calculateTargetPosition(targetX, targetY, targetZ, targetAbsoluteVx, targetAbsoluteVy, targetAbsoluteVz, tick)
-    local t = tick / TICK_PER_SEC
-    return targetX + targetAbsoluteVx * t,
-        targetY + targetAbsoluteVy * t,
-        targetZ + targetAbsoluteVz * t
+function calculateTargetPosition(selfX, selfY, selfZ,
+                                 selfVX, selfVY, selfVZ,
+                                 targetX, targetY, targetZ,
+                                 targetVx, targetVy, targetVz,
+                                 tick)
+    local st = SELF_DELAY_COMPENSATION / TICK_PER_SEC
+    selfX, selfY, selfZ = selfX + st * selfVX, selfY + st * selfVY, selfZ + st * selfVZ
+
+    local t = tick / TICK_PER_SEC + TARGET_DELAY_COMPENSATION
+    return targetX - selfX + targetVx * t,
+        targetY - selfY + targetVy * t,
+        targetZ - selfZ + targetVz * t
 end
 
-function convertGlobalXZToRelative(x, z, yaw)
-    return x * cos(yaw) - z * sin(yaw), z * cos(yaw) + x * sin(yaw)
-end
+function calAngleDiff2D(target, current)
+    -- Function to calculate the dot product
+    local function dotProduct(v1, v2)
+        return v1[1] * v2[1] + v1[2] * v2[2]
+    end
+    local function magnitude(v)
+        return (v[1] ^ 2 + v[2] ^ 2) ^ 0.5
+    end
 
-function convertLocalToRelativeValues(x, y, z, yaw, b)
-    -- transform from local value to global relative value
-    local globalX, globalY, globalZ = EularRotate({ x, y, z }, b)
-    local rx, rz = convertGlobalXZToRelative(globalX, globalZ, yaw)
-    return rx, globalY, rz
+    local dot = dotProduct(target, current)
+    local mag1 = magnitude(target)
+    local mag2 = magnitude(current)
+    if mag1 == 0 or mag2 == 0 then
+        return 0
+    end
+
+    -- Calculate the angle using the dot product and magnitudes
+    local angle = acos(dot / (mag1 * mag2))
+    if target[1] * current[2] - target[2] * current[1] < 0 then
+        return -angle
+    else
+        return angle
+    end
 end
 
 function onTick()
-    if IN(18) == 1 and IN(2) == 2 then
-        -- locked and use GSH-30-1
-        if IB(1) then
-            -- update calculation
-            local yaw = IN(12)
-
-            local selfVX, selfVY, selfVZ = IN(1), IN(2), IN(3)
-            local targetX, targetY, targetZ = IN(4), IN(5), IN(6)
-            local targetVx, targetVy, targetVz = IN(7), IN(8), IN(9)
-            local windSpeedX, windSpeedZ = IN(10), IN(11)
-
-            -- cal target offset
-            local ngeTick = 0
-            local posTick = 300
-            local travelTicks
-            local gap = PRECISION
-            local iter = 0
-            local x, y, z = 0, 0, 0
-
-            while abs(gap) >= PRECISION and iter <= MAX_ITER_COUNT do
-                iter = iter + 1
-                travelTicks = (posTick + ngeTick) / 2
-                x, y, z = calculateTargetPosition(
-                    targetX, targetY, targetZ,
-                    targetVx, targetVy, targetVz,
-                    travelTicks)
-                gap = calculateGap(MUZZEL_VELOCITY, x, y, z, travelTicks, windSpeedX, windSpeedZ, selfVX, selfVY, selfVZ)
-
-                if gap > 0 then
-                    posTick = travelTicks
-                else
-                    ngeTick = travelTicks
-                end
-            end
-
-            if abs(gap) >= PRECISION then
-                -- target cannot reach
-                OB(1, false) -- can reach target?
-                ON(1, 0)     -- muzzel offset x (rad)
-                ON(2, 0)     -- muzzel offset y (rad)
-                return
-            end
-
-            local velocityX, velocityY, velocityZ = calculateVelocity(
-                x, y, z, travelTicks,
-                windSpeedX, windSpeedZ,
-                selfVX, selfVY, selfVZ)
-            local yawTarget =
-                yaw + atan(velocityX / velocityZ)
-            local xz = (velocityX ^ 2 + velocityZ ^ 2) ^ 0.5
-            local lx, ly, lz = EularRotate({ xz * sin(yawTarget), velocityY, xz * cos(yawTarget) }, B)
-
-            OB(1, true)                              -- can reach target?
-            ON(1, atan(lx, lz))                      -- muzzel offset x (rad)
-            ON(2, atan(ly, (lx ^ 2 + lz ^ 2) ^ 0.5)) -- muzzel offset y (rad)
-        end
-    else
+    if not IB(1) then
+        -- BC off, set default values
         OB(1, false) -- can reach target?
         ON(1, 0)     -- muzzel offset x (rad)
         ON(2, 0)     -- muzzel offset y (rad)
+        return
+    end
+    if IB(2) then
+        -- update calculation pulse
+        local selfVX, selfVY, selfVZ = IN(1), IN(2), IN(3) -- local speed
+        -- calculate muzzelVelocity
+        local muzzelVelocity = (selfVX ^ 2 + selfVY ^ 2 + (selfVZ + MUZZEL_VELOCITY) ^ 2) ^ 0.5
+
+        local selfX, selfY, selfZ = IN(4), IN(5), IN(6)
+        local targetX, targetY, targetZ = IN(7), IN(8), IN(9)
+        local targetVx, targetVy, targetVz = IN(10), IN(11), IN(12)
+        local windSpeedX, windSpeedZ = IN(13), IN(14)
+
+        -- cal target offset
+        local ngeTick = 0
+        local posTick = MAX_TICK
+        local travelTicks
+        local gap = PRECISION
+        local iter = 0
+        local x, y, z = 0, 0, 0
+
+        while abs(gap) >= PRECISION and iter <= MAX_ITER_COUNT do
+            iter = iter + 1
+            travelTicks = (posTick + ngeTick) / 2
+            x, y, z = calculateTargetPosition(
+                selfX, selfY, selfZ,
+                selfVX, selfVY, selfVZ,
+                targetX, targetY, targetZ,
+                targetVx, targetVy, targetVz,
+                travelTicks)
+            gap = calculateGap(muzzelVelocity, x, y, z, travelTicks, windSpeedX, windSpeedZ)
+
+            if gap > 0 then
+                posTick = travelTicks
+            else
+                ngeTick = travelTicks
+            end
+        end
+
+        if abs(gap) >= PRECISION then
+            -- target cannot reach
+            OB(1, false) -- can reach target?
+            ON(1, 0)     -- muzzel offset x (rad)
+            ON(2, 0)     -- muzzel offset y (rad)
+            return
+        end
+
+        -- get calculated muzzelVelocity(global)
+        local velocityX, velocityY, velocityZ = calculateVelocity(x, y, z, travelTicks, windSpeedX, windSpeedZ)
+        -- convert to local muzzelVelocity
+        velocityX, velocityY, velocityZ = EularRotate({ velocityX, velocityY, velocityZ },
+            Eular2RotMat({ IN(15), IN(17), IN(16) }))
+
+        -- cal local yaw offset
+        local yawOffset = calAngleDiff2D({ velocityX, velocityZ }, { selfVX, selfVZ + MUZZEL_VELOCITY })
+        -- cal local pitch offset
+        local pitchOffset = -calAngleDiff2D(
+            { (velocityX ^ 2 + velocityZ ^ 2) ^ 0.5, velocityY },
+            { (selfVX ^ 2 + (selfVZ + MUZZEL_VELOCITY) ^ 2) ^ 0.5, selfVY })
+
+        OB(1, true)        -- can reach target?
+        ON(1, yawOffset)   -- muzzel yaw offset (rad)
+        ON(2, pitchOffset) -- muzzel pitch offset (rad)
     end
 end
+
+-- for test
+-- onTick()
---
+++ j11/WeaponSystem/GSH_30_1/BallisticComputerTestData.lua
@@ -0,0 +1,97 @@
+-- Machine Gun
+-- Muzzle velocity (m/s): 800
+-- Drag Coefficient: ?
+-- Gravity: 30 m/s^2, 0.5m/s/t
+-- Despawn Timer (ticks): 300
+-- Despawn Speed (m/s): 50
+
+-- Light Autocannon
+-- Muzzle velocity (m/s): 1000
+-- Drag Coefficient: 0.02
+-- Gravity: 30 m/s^2, 0.5m/s/t
+-- Despawn Timer (ticks): 300
+-- Despawn Speed (m/s): 50
+
+-- Rotary Autocannon
+-- Muzzle velocity (m/s): 1000
+-- Drag Coefficient: 0.01
+-- Gravity: 30 m/s^2, 0.5m/s/t
+-- Despawn Timer (ticks): 300
+-- Despawn Speed (m/s): 50
+
+-- Heavy Autocannon
+-- Muzzle velocity (m/s): 900
+-- Drag Coefficient: 0.005
+-- Gravity: 30 m/s^2, 0.5m/s/t
+-- Despawn Timer (ticks): 600
+-- Despawn Speed (m/s): 50
+
+-- Battle Cannon
+-- Muzzle velocity (m/s): 800m
+-- Drag Coefficient: 0.002
+-- Gravity: 30 m/s^2, 0.5m/s/t
+-- Despawn Timer (ticks): 3600
+-- Despawn Speed (m/s): N/A (50m/s underwater)
+
+-- Artillery Cannon
+-- Muzzle velocity (m/s): 700
+-- Drag Coefficient: 0.001
+-- Gravity: 30 m/s^2, 0.5m/s/t
+-- Despawn Timer (ticks): 3600
+-- Despawn Speed (m/s): N/A (50m/s underwater)
+
+-- Big Bertha Cannon
+-- Muzzle velocity (m/s): 600
+-- Drag Coefficient: 0.0005
+-- Gravity: 30 m/s^2, 0.5m/s/t
+-- Despawn Timer (ticks): 3600
+-- Despawn Speed (m/s): N/A (50m/s underwater)
+-- Rocket Launcher
+-- Muzzle velocity (m/s): ? (This guy accelerates for a certain amount of time)
+-- Drag Coefficient: 0.003
+-- Gravity: 30 m/s^2, 0.5m/s/t
+-- Despawn Timer (ticks): 3600
+-- Despawn Speed (m/s): N/A (50m/s underwater)
+-- - Larger caliber projectiles receive proportionally less force from the wind
+
+input = {
+    getNumber = function(i)
+        local data = {
+            0, 0, 300,  -- self v
+            0, 0, 1000, -- self pos
+            0, 0, 2000, -- target pos
+            0, 0, 0,    -- target v
+            0, -30,     -- windSpeedX, windSpeedZ
+            0, 0, 0     -- self rotation
+        }
+        return data[i]
+    end,
+    getBool = function(i)
+        local data = { true, true }
+        return data[i]
+    end
+}
+output = {
+    setNumber = function(i, num)
+        print(string.format("set num#%d: %f", i, num))
+    end,
+    setBool = function(i, val)
+        print(string.format("set bool#%d: %s", i, tostring(val)))
+    end
+}
+property = {
+    getNumber = function(key)
+        local data = {
+            ["Tick per Sec"] = 60,
+            ["[BC]Drag Coefficient"] = 0.01,
+            ["[BC]Muzzel Velocity"] = 1000,
+            ["[BC]Gravity"] = 30,
+            ["[BC]Precision"] = 0.0001,
+            ["[BC]Max Iterations"] = 40,
+            ["[BC]Max Tick"] = 300,
+            ["[BC]Target Delay Compensation"] = 0,
+            ["[BC]Self Delay Compensation"] = 0
+        }
+        return data[key]
+    end
+}
